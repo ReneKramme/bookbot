18309136cc55a25e7ea7b055e2e1b1319dd1f28e
René Kramme <renekramme@gmail.com>
2025-12-27 10:57:32 +0100

    ignore data, tekstfil til streng

---
+++ main.py
@@ -0,0 +1,10 @@
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
+    
+def main():
+    book_path = "./books/frankenstein.txt"
+    book_text = get_book_text(book_path)
+    print(book_text)
+    
+main()
