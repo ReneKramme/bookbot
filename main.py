7c51cc3f363023c4ce86bd6cec4edcfe801d3575
René Kramme <renekramme@gmail.com>
2025-12-29 21:32:07 +0100

    Ny fordeling af funktioner

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
 # import funktion til tekstanalyse
-from stats import * #get_book_text, count_words
+from stats import count_words, count_each_letter
 
 def main():
     book_path = "./books/frankenstein.txt"
     book_text = get_book_text(book_path)
     num_words = count_words(book_text)
     print(f"Found {num_words} total words")
+    letters = count_each_letter(book_text)
+    print(f" Disse bogstaver findes i bogen: {letters}")
+    
+
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
+
     
 main()
--- stats.py
+++ stats.py
@@ -1,7 +1,14 @@
-def get_book_text(path):
-    with open(path) as f:
-        return f.read()
-    
 def count_words(text):
     words = text.split()
     return len(words)
+
+def count_each_letter(text):
+    letters = {}
+    for letter in text:
+       # if letter.isalpha():
+            letter = letter.lower()
+            if letter in letters:
+                letters[letter] += 1
+            else:
+                letters[letter] = 1
+    return letters
