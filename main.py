b3b2a39c7abd9e125ab1459b66233abb811c394d
René Kramme <renekramme@gmail.com>
2025-12-30 23:17:50 +0100

    tager argument og tæller bogstaver

--- main.py
+++ main.py
@@ -1,15 +1,27 @@
-# import funktion til tekstanalyse
-from stats import count_words, count_each_letter
+# import funktioner til tekstanalyse
+from stats import count_words, count_each_letter, dict_to_sorted_list
+import sys
 
 def main():
-    book_path = "./books/frankenstein.txt"
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    book_path = sys.argv[1]
     book_text = get_book_text(book_path)
     num_words = count_words(book_text)
     print(f"Found {num_words} total words")
     letters = count_each_letter(book_text)
-    print(f" Disse bogstaver findes i bogen: {letters}")
+    print(f"""
+============ BOOKBOT ============
+Analyzing book found at {book_path}
+----------- Word Count ----------
+Found {num_words} total words
+--------- Character Count -------""")
     
-
+    sorteret = dict_to_sorted_list(letters)
+    for bogstav in sorteret:
+       print(f"{bogstav["char"]}: {bogstav["num"]}")
+    
 def get_book_text(path):
     with open(path) as f:
         return f.read()
--- stats.py
+++ stats.py
@@ -5,10 +5,18 @@ def count_words(text):
 def count_each_letter(text):
     letters = {}
     for letter in text:
-       # if letter.isalpha():
+        if letter.isalpha():
             letter = letter.lower()
             if letter in letters:
                 letters[letter] += 1
             else:
                 letters[letter] = 1
     return letters
+
+def dict_to_sorted_list(dict):
+    unsorted_list = []
+    for key, value in dict.items():
+        unsorted_list.append({"char": key, "num": value})
+    unsorted_list.sort(key=lambda x: x["num"], reverse=True)
+    return unsorted_list
+
